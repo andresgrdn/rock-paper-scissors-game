64169605224af47ecdabb3b6b985081efd313722
Andres Gordon <gordon_andres@yahoo.es>
2022-06-13 19:21:23 -0500

    Added basic game functionality

---
+++ scripts/game.js
@@ -0,0 +1,75 @@
+let score = 0;
+let pieces = ['rock', 'paper', 'scissors'];
+
+// Helpers
+function chooseRandElement(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+
+  return array[randomIndex]
+}
+
+let rules = {
+  rock: {
+    'wins': 'scissors',
+    'lose': 'paper',
+  },
+  paper: {
+    'wins': 'rock',
+    'lose': 'scissors',
+  },
+  scissors: {
+    'wins': 'paper',
+    'lose': 'rock',
+  },
+}
+
+let playerChoose = prompt(`Please choose one of the following options => ${pieces}`);
+let cpuChoose = chooseRandElement(pieces.filter(piece => piece != playerChoose));
+
+console.log(`Player: ${playerChoose} <==> Cpu: ${cpuChoose}`);
+console.log("Player wins ", rules[playerChoose].wins === cpuChoose);
+console.log("Player Lose ", rules[playerChoose].lose === cpuChoose);
+
+/** Rules */
+/**
+ * Both cannot use the same piece at the time
+ * 
+ * The player choose a piece from the pieces available
+ * 
+ * The cpu choose a random piece from the pieces available
+ * 
+ * If the player choose rock
+ *  cpu choosed from the pieces (paper, scissors)
+ *  if cpu choose paper then cpu wins
+ *  if cpu choose scissors then cpu loses
+ * 
+ * If the player choose paper
+ *  cpu choosed from the pieces (rock, scissors)
+ *  if cpu choose scissors then cpu wins
+ *  if cpu choose rock then cpu loses
+ * 
+ * If the player choose scissors
+ *  cpu choosed from the pieces (paper, rock)
+ *  if cpu choose rock then cpu wins
+ *  if cpu choose paper then cpu loses
+ * 
+ * when the player wins the score adds one
+ * but if the cpu wins the score low one
+ * 
+ * Play again is returning to the first view
+ * but the score is not updated
+
+ Requirements
+
+ Score
+ Rules
+ 
+ You Picked
+ The House Picked
+ 
+ You Win
+ You Lose
+ 
+ Play Again
+
+ */
